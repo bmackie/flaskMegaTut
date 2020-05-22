e792fd0e7c7a2147fca4ffa9f28d122ac2fa4309
bmackie <the.bmackie@gmail.com>
2020-05-22 06:43:48 -0400

    Restrucred application with blueprints

--- update-babel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pybabel extract -F babel.cfg -k _l -o messages.pot .
-pybabel update -i messages.pot -d app/translations
--- update-requirements.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm requirements.txt
-pip freeze >> requirements.txt
